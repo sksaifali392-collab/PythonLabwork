aa72f93f23621b76476921776cf06900e8ef436c
Sk Saif Ali <sksaifali392@gmail.com>
2025-08-04 10:49:42 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+num1= int(input("Enter the first number: "))
+
+
+num2 = int(input("Enter the second number: "))
+
+sum_result = num1 + num2
+sum_result2= num1 - num2
+sum_result3= num1 * num2
+sum_result4= num1 / num2
+
+print(f"The sum of num1 and num2 is: {sum_result}")
+print(f"The sub of num1 and num2 is: {sum_result2}")
+print(f"The mul of num1 and num2 is: {sum_result3}")
+print(f"The div of num1 and num2 is: {sum_result4}")
